74acb93d8f49b464e7db58a62ea4d7a42fcd26e5
Pedro Dias <petermdias@gmail.com>
2015-06-30 18:23:11 +0100

    vm.monitoring, version bump

--- test/vm.js
+++ test/vm.js
@@ -9,7 +9,7 @@ describe("#vm", function() {
     it("should create a vm", function(done) {
       this.timeout(5000);
 
-      one.createVM('GRAPHICS=[TYPE="vnc",LISTEN="0.0.0.0"]\nMEMORY="1024"\n FROM_APP="53e767ba8fb81d6a69000001"\nVCPU="1"\nFROM_APP_NAME="CentOS 6.5 - KVM"\nOS=[ARCH="x86_64"]\n NIC=[NETWORK="private"]\nLOGO="images/logos/centos.png"\nCPU="0.5"\n DISK=[IMAGE="CentOS-6.5-one-4.8",IMAGE_UNAME="oneadmin"]\n', false, function(err, vm) {
+      one.createVM('GRAPHICS=[TYPE="vnc",LISTEN="0.0.0.0"]\nMEMORY="512"\nVCPU="1"\nOS=[ARCH="x86_64"]\n NIC=[NETWORK="cloud"]\nCPU="0.5"\n DISK=[IMAGE="ttylinux",IMAGE_UNAME="oneadmin"]\n', false, function(err, vm) {
         expect(err).to.be.null;
         expect(vm).to.be.ok;
 
